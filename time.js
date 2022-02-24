8e1291ef2b6fa5c4ab2729229ec181f2f41d3fba
Omar Ortega <srhazer.page@gmail.com>
2022-02-23 16:49:42 -0800

    show days

--- time.js
+++ time.js
@@ -1,15 +1,17 @@
 window.onload = () => {
-   h = 22;   //
-   m = 22;   // hora en que iniciara el cronometro
-   s = 22;   //
-   mls = 22; //
+   d = getDays("02/22/2022");
+   h = 0;   //
+   m = 0;   // hora en que iniciara el cronometro
+   s = 0;   //
+   mls = 0; //
    timeStarted = 0;
+   days = document.getElementById("days");
    time1 = document.getElementById("time1");
    time2 = document.getElementById("time2");
-   btnStart = document.getElementById("btn-start");
-   btnStart.addEventListener("click", start);
-   btnStop = document.getElementById("btn-stop");
-   btnStop.addEventListener("click", stop);
+   //btnStart = document.getElementById("btn-start");
+   //btnStart.addEventListener("click", start);
+   //btnStop = document.getElementById("btn-stop");
+   //btnStop.addEventListener("click", stop);
    //btnReset = document.getElementById("btn-reset"); // descomentar si se desea activar el reset
    //btnReset.addEventListener("click", reset);
 
@@ -32,13 +34,17 @@ function write() {
       h++;
       m = 0;
    }
-   if (h > 24) h = 0;
+   if (h > 24){
+      d++;
+      h = 0;
+   }
 
    mlst = ('0' + mls).slice(-2);
    st = ('0' + s).slice(-2);
    mt = ('0' + m).slice(-2);
    ht = ('0' + h).slice(-2);
 
+   days.innerHTML = `${d} days`;
    time1.innerHTML = `${ht}:${mt}:`;
    time2.innerHTML = `${st}.${mlst}`;
 }
@@ -49,6 +55,22 @@ function start() {
    btnStart.removeEventListener("click", start);
 }
 
+
+function getDays(date){
+   var firstDate = new Date(date);
+   var dateNow = new Date();
+   
+   // To calculate the time difference of two dates
+   var Difference_In_Time = dateNow.getTime() - firstDate.getTime();
+   
+   console.log(Difference_In_Time);
+   // To calculate the no. of days between two dates
+   var Difference_In_Days = Difference_In_Time / (1000 * 3600 * 24);
+   
+   return Math.floor(Difference_In_Days);
+}
+
+/*
 function stop() {
    clearInterval(timeStarted);
    btnStart.addEventListener("click", start);
@@ -57,8 +79,8 @@ function stop() {
 /*
 function reset() {
    clearInterval(timeStarted);
-   time1.innerHTML = "22:22:"    // En caso de activar el reset, modificar esta hora
-   time2.innerHTML = "22.22"
+   time1.innerHTML = "00:00:"
+   time2.innerHTML = "00.00"
    h = 0;
    m = 0;
    s = 0;
