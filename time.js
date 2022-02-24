f1aaaf82a8710f0d11b08eded3d29d4d5b5e9ddb
Omar Ortega <srhazer.page@gmail.com>
2022-02-23 19:46:07 -0800

    ya quedo 1.0.0.1

--- time.js
+++ time.js
@@ -1,19 +1,15 @@
+
+
 window.onload = () => {
-   d = getDays("02/22/2022");
    h = 0;   //
    m = 0;   // hora en que iniciara el cronometro
    s = 0;   //
    mls = 0; //
    timeStarted = 0;
+   d = getDays("02/22/2022 16:25:20");
    days = document.getElementById("days");
    time1 = document.getElementById("time1");
    time2 = document.getElementById("time2");
-   //btnStart = document.getElementById("btn-start");
-   //btnStart.addEventListener("click", start);
-   //btnStop = document.getElementById("btn-stop");
-   //btnStop.addEventListener("click", stop);
-   //btnReset = document.getElementById("btn-reset"); // descomentar si se desea activar el reset
-   //btnReset.addEventListener("click", reset);
 
    start();
 };
@@ -44,47 +40,44 @@ function write() {
    mt = ('0' + m).slice(-2);
    ht = ('0' + h).slice(-2);
 
-   days.innerHTML = `${d} days`;
+   days.innerHTML = `${d} dias`;
    time1.innerHTML = `${ht}:${mt}:`;
    time2.innerHTML = `${st}.${mlst}`;
 }
 
-function start() {
-   write();
-   timeStarted = setInterval(write, 10);
-   btnStart.removeEventListener("click", start);
-}
-
-
 function getDays(date){
    var firstDate = new Date(date);
    var dateNow = new Date();
-   
+
    // To calculate the time difference of two dates
    var Difference_In_Time = dateNow.getTime() - firstDate.getTime();
-   
-   console.log(Difference_In_Time);
+
    // To calculate the no. of days between two dates
-   var Difference_In_Days = Difference_In_Time / (1000 * 3600 * 24);
-   
-   return Math.floor(Difference_In_Days);
+   var Difference_In_Days = Math.floor( Difference_In_Time / (1000 * 3600 * 24) );
+   Difference_In_Time -= Difference_In_Days*1000*3600*24;
+
+   var Difference_In_Hours = Math.floor( Difference_In_Time / (1000 * 3600) );
+   Difference_In_Time -= Difference_In_Hours*1000*3600;
+
+   var Difference_In_Minutes = Math.floor( Difference_In_Time / (1000 * 60) );
+   Difference_In_Time -= Difference_In_Minutes*1000*60;
+
+   var Difference_In_Seconds = Math.floor( Difference_In_Time / 1000);
+
+   console.log("Days  : "+Difference_In_Days);
+   console.log("Hours : "+Difference_In_Hours);
+   console.log("Mins  : "+Difference_In_Minutes);
+   console.log("Secs  : "+Difference_In_Seconds);
+   h = Difference_In_Hours;
+   m = Difference_In_Minutes;
+   s = Difference_In_Seconds;
+
+   return Difference_In_Days;
 }
 
-/*
-function stop() {
-   clearInterval(timeStarted);
-   btnStart.addEventListener("click", start);
-}
 
-/*
-function reset() {
-   clearInterval(timeStarted);
-   time1.innerHTML = "00:00:"
-   time2.innerHTML = "00.00"
-   h = 0;
-   m = 0;
-   s = 0;
-   mls = 0;
-   btnStart.addEventListener("click", start);
+
+function start() {
+   write();
+   timeStarted = setInterval(write, 10);
 }
-*/
